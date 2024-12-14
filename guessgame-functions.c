1d34e8907aaf7d4131969bca2030947b49b72499
WECLEY <wecwhengithub7.anything491@slmail.me>
2024-12-14 20:11:32 +0100

    Improved readability and security

--- guessgame-functions.c
+++ guessgame-functions.c
@@ -1,73 +1,42 @@
 
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include "guessgame-functions.h"
-#include "guessgame-messages.h"
 
-char *getuniq(char *word_result){
-	int word_result_size = strlen(word_result);
+//get unique characters in a string, returns a pointer
+char *getuniq(char *string){
+	int string_len = strlen(string);
+	char *uniq_chars_buffer;
+	while ( (uniq_chars_buffer = malloc(string_len *sizeof(char)))  == NULL );
+	//to stores the chars seeked from goal string
 
-	//to stores the chars as it seeks them from result str
-	char *word_uniq_buffer = malloc(word_result_size *sizeof(char));
-
-	//store a new character from result str
-	//then strcopy to buffer to a new smaller string size
-
-	for(int p=0, u=0, n=0; p < word_result_size; p++)
+	for(int p=0, u=0, n=0; p < string_len; p++) //loop over the goal string
 	{
-		for(; word_uniq_buffer[n] ; n++)
-			if (word_result[p] == word_uniq_buffer[n])
+		for(; uniq_chars_buffer[n] ; n++) //compare every character in string with
+										//ones in uniq_buffer, if exists, skip
+										//if not. (reached end of uniq_buffer),
+										//add the looked up character
+										//uniq_chars_buffer[n] to uniq_buffer.
+			if (string[p] == uniq_chars_buffer[n])
 				{n=0; break; }
-		if(n == (int)strlen(word_uniq_buffer))
+		if(n == (int)strlen(uniq_chars_buffer))
 		{
-			word_uniq_buffer[u++] = word_result[p];
+			uniq_chars_buffer[u++] = string[p];
 			n=0;
 		}
 	}
-	char *ptr = malloc(strlen(word_uniq_buffer) *sizeof(char));
-	strcpy (ptr, word_uniq_buffer);
-	free(word_uniq_buffer);
+	//use less memory
+	char *ptr;
+	while ( (ptr = malloc(strlen(uniq_chars_buffer) *sizeof(char))) == NULL );
+	strcpy (ptr, uniq_chars_buffer);
+	free(uniq_chars_buffer);
+
 	return ptr;
 }
 
-void exposeword(char *word_result, char *word_hidden, char answer_input)
+void exposechar(char *wrd_exposed, char *wrd_hidden, char keychar)
 {
-	for(int i = 0; word_result[i]; i++)
-		if( word_result[i] == answer_input )
-			word_hidden[i] = word_result[i];
-}
-
-void start_guess(char *word_result, char *word_uniq, char *word_hidden,
-				int *lives, int *bingos){
-	char answer_input; //store guessed letter
-
-	printf("try:");
-
-	while((answer_input = fgetc(stdin)) != '\n' && *lives != 0 && *bingos != 0){
-		//  add returning terminal cursor to position!
-		int correct = 0; //exists or not
-		if( !(isdigit(answer_input) || isalpha(answer_input) ||answer_input ==' ') )
-			continue;
-		//checks if the taken character exists, and expose it in word_hidden
-		//
-		for(int i = 0; word_uniq[i]; i++){
-			if( word_uniq[i] == answer_input){
-
-				correct = 1;
-				(*bingos)--;
-
-				word_uniq[i] = '\200';
-				exposeword(word_result, word_hidden, answer_input);
-				printf(M_CRCT,answer_input, word_hidden);
-				break;
-			}
-		}
-		if( correct == 0)
-		{
-			(*lives)--;
-			printf(M_WRNG ,answer_input, *lives);
-		}
-	}
+	for(int i = 0; wrd_exposed[i]; i++)
+		if( wrd_exposed[i] == keychar )
+			wrd_hidden[i] = wrd_exposed[i];
 }
--- guessgame-main.c
+++ guessgame-main.c
@@ -1,41 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "guessgame-functions.h"
 #include "guessgame-messages.h"
 
+void start_guess(char *wrd_goal, char *wrd_uniq, char *wrd_hidden,
+				int *lives, int *bingos){
+	char answer_input; //store guessed letter
+
+	printf("try:");
+
+	//as long as there are plays retries , get a pending character
+	while((answer_input = fgetc(stdin)) != '\n' && *lives != 0 && *bingos != 0){
+		int correct = 0; //exists or not
+
+		//if the pending character didnt meet the requirments, redo the while
+		if( !(isdigit(answer_input) || isalpha(answer_input) ||answer_input ==' ') )
+			continue;
+
+		//checks if the taken character exists in wrd_uniq
+		//if yes, update values and expose it in wrd_hidden
+		//if no, lose the try
+		for(int i = 0; wrd_uniq[i]; i++){
+			if( wrd_uniq[i] == answer_input){
+
+				correct = 1;
+				(*bingos)--;
+
+				wrd_uniq[i] = '\200';
+				exposechar(wrd_goal, wrd_hidden, answer_input);
+				printf(M_CRCT,answer_input, wrd_hidden);
+				break;
+			}
+		}
+		if( correct == 0)
+		{
+			(*lives)--;
+			printf(M_WRNG ,answer_input, *lives);
+		}
+	}
+}
+
 int main(){
-	char word_result[124];//carries the full name "abdsamad"
+	char wrd_goal[64];//carries the full name
+
+
+	//ask for and get the goal text
 	printf(CLR_DISP RST_CUR "enter a string: ");
-	fgets(word_result, 100, stdin);// get input to word_result
+	fgets(wrd_goal, 64, stdin);
 	printf(CLR_DISP RST_CUR);
-	word_result[strcspn(word_result, "\n")] = 0;// no tailing "\n"
+	wrd_goal[strcspn(wrd_goal, "\n")] = 0;// no tailing "\n"
 
-	char *word_uniq = getuniq(word_result);//unique chars from input "abdsm"
-	char word_hidden[strlen(word_result)];// the "-----"
+	//prepare these strings
+	char *wrd_uniq = getuniq(wrd_goal);
+	char wrd_hidden[strlen(wrd_goal)];// the "-----", am i better off mallocating?...
 
-	int i= 0;//bad fix, find root issue
-	for(; i < (int)strlen(word_result); i++) // create the "-----"
-		word_hidden[i] = '-';
-	word_hidden[i] = 0;
-	// used to be
-	// for(int i= 0; i < (int)strlen(word_result); i++) // create the "-----"
-	// 	word_hidden[i] = '-';
+	int i= (int)strlen(wrd_goal);//bad fix, find root issue
+	wrd_hidden[i--] = 0;
+	for(; i >= 0; i--) // create the "-----"
+		wrd_hidden[i] = '-';
 
-	int bingos = strlen(word_uniq);// how many correct words to guess and win
+	int bingos = strlen(wrd_uniq);// how many correct wrds to guess and win
 	int lives = 10;// tries
 
-	printf(M_GUESS, word_hidden,(int)strlen(word_hidden));
 //======[	begin	]=======
-	while(lives != 0 && bingos != 0){
-		start_guess(word_result, word_uniq, word_hidden, &lives, &bingos);
-	}
-
+	printf(M_GUESS, wrd_hidden,(int)strlen(wrd_hidden));
+	while(lives != 0 && bingos != 0)//self explainatory ngl
+		start_guess(wrd_goal, wrd_uniq, wrd_hidden, &lives, &bingos);
 //======[	 end	]=======
 	if (bingos == 0)
 		printf("%s\n",M_RUNWON);
 	else
 		printf("%s\n",M_RUNLOS);
-	free(word_uniq);
+	free(wrd_uniq);
 	return 0;
 }
