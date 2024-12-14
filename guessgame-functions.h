fe6d77c335d3530777de389fb69e9bf42271ef5e
WECLEY <wecwhengithub7.anything491@slmail.me>
2024-12-14 20:12:21 +0100

    ryosa

--- guessgame-functions.h
+++ guessgame-functions.h
@@ -1,7 +1,7 @@
 #ifndef GUESSGAME_FUNCTIONS_H
 #define GUESSGAME_FUNCTIONS_H
-	char *getuniq(char *word_result);
-	void exposeword(char *word_result, char *word_hidden, char answer_input);
-	void start_guess(char *word_result, char *word_uniq, char *word_hidden,
+	char *getuniq(char *string);
+	void exposechar(char *wrd_exposed, char *wrd_hidden, char keychar);
+	void start_guess(char *wrd_goal, char *wrd_uniq, char *wrd_hidden,
 				int *lives, int *bingos);
 #endif
